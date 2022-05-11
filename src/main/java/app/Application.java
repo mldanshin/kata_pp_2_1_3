62579dadd3ca45d45e2fbe03910ed78d8344da90
Danshin Maxim <mail@danshin.net>
2022-05-11 12:07:39 +0700

    decision

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -2,6 +2,7 @@ package app;
 
 import app.config.AppConfig;
 import app.model.AnimalsCage;
+import org.springframework.beans.factory.BeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
